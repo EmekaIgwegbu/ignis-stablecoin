c1b60e2fd0de51626262e58b150a9633855eae63
Emeka Igwegbu <emeka.igwegbu@gmail.com>
2024-07-16 23:45:23 -0700

    Define context structs for the ignis_stablecoin program

--- programs/ignis-stablecoin/src/lib.rs
+++ programs/ignis-stablecoin/src/lib.rs
@@ -4,84 +4,92 @@ use anchor_spl::token::{self, Mint, MintTo, Token, TokenAccount, Transfer};
 declare_id!("Bou8TKf8G9iJQoZMptYtFHrpgvEjG4DTTXo8sxShLsht");
 
 #[program]
-pub mod algorithmic_stablecoin {
+pub mod ignis_stablecoin {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, supply: u64) -> Result<()> {
-        let stablecoin = &mut ctx.accounts.stablecoin;
-        stablecoin.supply = supply;
-        stablecoin.price = 1.0; // Initial price
+    pub fn redeem_for_sister_coin(ctx: Context<Redeem>) -> Result<()> {
         Ok(())
     }
 
-    pub fn mint(ctx: Context<MintStablecoin>, amount: u64) -> Result<()> {
-        let cpi_accounts = MintTo {
-            mint: ctx.accounts.mint.to_account_info().clone(),
-            to: ctx.accounts.token_account.to_account_info().clone(),
-            authority: ctx.accounts.authority.to_account_info().clone(),
-        };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        token::mint_to(cpi_ctx, amount)?;
+    // TODO: I'm starting to think that this initialise function shouldn't be here since it's not a public-facing API. Or I need
+    // to find a way to restrict access to the reserve authority, which would mean that the reserve authority would need to be predefined.
+    // I think setting up the reserve wallet manually might make more sense. This way I can prevent a situation where a user passes in random token accounts that are supposedly
+    // the ignis_reserve and ventura_reserve --> in the current implementation I think this would work as long as these accounts' mints match those of the user accounts.
+    // Should the reserve wallet, mint account or token accounts somehow be derive from the program ID? Or maybe IgnisStablecoin should be derived from the program ID?
 
-        let stablecoin = &mut ctx.accounts.stablecoin;
-        stablecoin.supply += amount;
+    pub fn initialise(ctx: Context<Initialise>, initial_supply: u64, decimals: u8) -> Result<()> {
+        let ignis_stablecoin = &mut ctx.accounts.ignis_stablecoin;
+        // TODO: Invoke token program here to create ignis mint
+        // ignis_stablecoin.mint =
+        // ignis_stablecoin.name = "Ignis";
+        // ignis_stablecoin.symbol = "IGS";
+        ignis_stablecoin.peg = 1.0;
+        ignis_stablecoin.reserve_authority = ctx.accounts.reserve_authority.key();
 
+        // TODO: Invoke token program here to mint new ignis and move it to the newly created ignis reserve (ignis token account)
+        // ignis_stablecoin.reserve =
         Ok(())
     }
 
-    pub fn burn(ctx: Context<BurnStablecoin>, amount: u64) -> Result<()> {
-        let cpi_accounts = Transfer {
-            from: ctx.accounts.from.to_account_info().clone(),
-            to: ctx.accounts.to.to_account_info().clone(),
-            authority: ctx.accounts.authority.to_account_info().clone(),
-        };
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        token::transfer(cpi_ctx, amount)?;
-
-        let stablecoin = &mut ctx.accounts.stablecoin;
-        stablecoin.supply -= amount;
-
+    pub fn mint(ctx: Context<MintIgnis>, amount: u64) -> Result<()> {
         Ok(())
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8 + 8)]
-    pub stablecoin: Account<'info, Stablecoin>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct MintStablecoin<'info> {
-    #[account(mut)]
-    pub stablecoin: Account<'info, Stablecoin>,
-    #[account(mut)]
-    pub mint: Account<'info, Mint>,
-    #[account(mut)]
-    pub token_account: Account<'info, TokenAccount>,
-    pub authority: Signer<'info>,
-    pub token_program: Program<'info, Token>,
-}
-
-#[derive(Accounts)]
-pub struct BurnStablecoin<'info> {
-    #[account(mut)]
-    pub stablecoin: Account<'info, Stablecoin>,
-    #[account(mut)]
-    pub from: Account<'info, TokenAccount>,
-    #[account(mut)]
-    pub to: Account<'info, TokenAccount>,
-    pub authority: Signer<'info>,
-    pub token_program: Program<'info, Token>,
+    pub fn burn(ctx: Context<BurnIgnis>, amount: u64) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[account]
-pub struct Stablecoin {
-    pub supply: u64,
-    pub price: f64,
+pub struct IgnisStablecoin {
+    pub name: [u8; 32],
+    pub symbol: [u8; 16],
+    pub mint: Pubkey,    // The address of the mint account
+    pub reserve: Pubkey, // The address of the token account that belongs to the ignis reserve
+    pub peg: f64,
+    pub reserve_authority: Pubkey,
+}
+
+#[derive(Accounts)]
+pub struct Initialise<'info> {
+    // Change this to include space for other fields
+    #[account(init, payer = reserve_authority, space = 8 + 32 + 16 + 32 + 8 + 32)]
+    pub ignis_stablecoin: Account<'info, IgnisStablecoin>,
+    #[account(mut)]
+    pub reserve_authority: Signer<'info>,
+    pub token_program: Program<'info, System>,
+    pub system_program: Program<'info, System>,
+    // TODO: does mint (account) need to be included here like
+    // https://beta.solpg.io/https://github.com/solana-developers/anchor-examples/tree/main/account-constraints/token ?
+}
+
+#[derive(Accounts)]
+pub struct Redeem<'info> {
+    #[account(mut, token::authority = user, token::mint = ignis_reserve.mint)]
+    pub user_ignis_account: Account<'info, TokenAccount>,
+    #[account(mut, token::authority = user, token::mint = ventura_reserve.mint)]
+    pub user_ventura_account: Account<'info, TokenAccount>,
+    #[account(mut, token::authority = reserve_authority)]
+    pub ignis_reserve: Account<'info, TokenAccount>,
+    #[account(mut, token::authority = reserve_authority)]
+    pub ventura_reserve: Account<'info, TokenAccount>,
+    pub user: Signer<'info>,
+    pub reserve_authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct MintIgnis<'info> {
+    #[account(mut, token::authority = reserve_authority)]
+    pub ignis_reserve: Account<'info, TokenAccount>,
+    pub reserve_authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct BurnIgnis<'info> {
+    #[account(mut, token::authority = reserve_authority)]
+    pub ignis_reserve: Account<'info, TokenAccount>,
+    pub reserve_authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
 }
